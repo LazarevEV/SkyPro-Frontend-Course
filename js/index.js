fce7379d42b2faaff7a53755087a28bc474aa522
LazarevEV <lazarev.evgeniy.nf@gmail.com>
2021-11-12 02:56:04 +0300

    Task 1 - Added: index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let product_name = 'PRODUCT_NAME_PLACHOLDER'
-let product_price = '99999'
+let product_name = 'PLACEHOLDER'
+let product_price = '999999'
 let str = `${product_name}, ${product_price} RUR`
 
 alert(str)
